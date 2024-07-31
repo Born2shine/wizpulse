f0f00daae9d84d23af3373b28d521147213c5b3a
Odoh Friday <odohfriday33@yahoo.com>
2024-07-31 15:24:16 +0100

    change onboarding route to home route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,13 +3,13 @@ import routesPath from "@/utils/routesPath";
 import { Route, Routes } from "react-router-dom";
 
 
-const { ONBOARDING } = routesPath;
+const { HOME } = routesPath;
 
 export default function AppRoute() {
   return (
     <>
       <Routes>
-        <Route index path={ONBOARDING} element={<Onboarding />} />
+        <Route index path={HOME} element={<Onboarding />} />
         {/* <Route index path={HOME} element={<Home />} /> */}
         {/* <Route index path={LOGIN} element={<Login />} /> */}
       </Routes>
